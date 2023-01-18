adf3b6ef4f38b36938de1a880bb39b41452f1476
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-18 01:02:13 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,6 +20,11 @@ app.use(cors({
   origin: ['http://playwell-online-exux.vercel.app'],
   allowedHeaders: ['authorization']
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://playwell-online-exux.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 // autoload routes
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
