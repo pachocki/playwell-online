fe192add00d599e47ba0d96d71ca96c0146b6a3b
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-20 00:19:38 +0100

    updt

--- client/src/pages/home/sections/about-courses/AboutCoursesMobile.jsx
+++ client/src/pages/home/sections/about-courses/AboutCoursesMobile.jsx
@@ -5,9 +5,12 @@ import { Link } from "react-router-dom";
 import Header from "../../../../components/header/Header";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Pagination } from "swiper";
+
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "../../../../styles.css";
 
 //Data
@@ -35,35 +38,41 @@ const AboutCoursesMobile = () => {
               spaceBetween: 10,
             },
           }}
+          pagination={true}
+          modules={[Pagination]}
           className="mySwipe  rounded-xl !pb-5 "
         >
           {CoursesDescriptionsMobile.map((item, id) => (
             <SwiperSlide key={item.id}>
               <div
                 className={
-                  " w-full h-full rounded-xl overflow-hidden relative z-10 bg-black" 
+                  " w-full h-full rounded-xl overflow-hidden relative z-10 bg-black"
                 }
               >
                 <div className="rgb ">
-                <div className="px-1 pt-1 screen:h-[30vh] tablet:h-[28vh]">
-                  <img src={item.image} alt="Fortnite" className="rounded-t-xl" />
-                </div>
-                <div className=" pt-3 pb-8 mobile:pb-4 screen:h-[51vh] tablet:h-full bg-black mx-1 mb-1 rounded-b-xl">
-                  <span className="text-3xl font-bold small-mobile:text-2xl">
-                    {item.title}
-                  </span>
-                  <p className="pt-3 text-left screen:px-4  tablet:px-2 text-xl tablet:text-[1rem] small-mobile:text-[0.95rem]">
-                    {item.description}
-                  </p>
-                  <div className="pt-5">
-                    <Link to={item.link}>
-                      <button className="font-bold  py-2 px-5 bg-red-700 hover:opacity-80 rounded-xl tablet:text-sm">
-                        Les mer
-                      </button>
-                    </Link>
+                  <div className="px-1 pt-1 screen:h-[30vh] tablet:h-[28vh]">
+                    <img
+                      src={item.image}
+                      alt="Fortnite"
+                      className="rounded-t-xl"
+                    />
+                  </div>
+                  <div className=" pt-3 pb-8 mobile:pb-4 screen:h-[51vh] tablet:h-full bg-black mx-1 mb-1 rounded-b-xl">
+                    <span className="text-3xl font-bold small-mobile:text-2xl">
+                      {item.title}
+                    </span>
+                    <p className="pt-3 text-left screen:px-4  tablet:px-2 text-xl tablet:text-[1rem] small-mobile:text-[0.95rem]">
+                      {item.description}
+                    </p>
+                    <div className="pt-5">
+                      <Link to={item.link}>
+                        <button className="font-bold  py-2 px-5 bg-red-700 hover:opacity-80 rounded-xl tablet:text-sm">
+                          Les mer
+                        </button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
               </div>
             </SwiperSlide>
           ))}
