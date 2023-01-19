5a62c316f33818f0c4e5b9b55222b6a4132bb000
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-19 15:59:00 +0100

    Update Subscription.jsx

--- client/src/pages/subscription/Subscription.jsx
+++ client/src/pages/subscription/Subscription.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import SubscriptionPlan from "../../components/subscription-plan/SubscriptionPlan";
+import { UserContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-//import Pricingtest from "../../components/pricing-card/Pricingtest";
 const Subscription = () => {
+  const [state, setState] = useContext(UserContext);
+  const [userSubscriptions, setUserSubscriptions] = useState([]);
+  const [prices, setPrices] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const navigate = navigate();
+
+  useEffect(() => {
+    fetchPrices();
+  }, []);
+
+  useEffect(() => {
+    let result = [];
+    const check = () =>
+      state &&
+      state.user &&
+      state.user.subscriptions &&
+      state.user.subscriptions.map((sub) => {
+        result.push(sub.plan.id);
+      });
+    check();
+    setUserSubscriptions(result);
+  }, [state && state.user]);
+
+  useEffect(() => {
+    const isPaused = () => {
+      state &&
+        state.user &&
+        state.user.subscriptions &&
+        state.user.subscriptions.resumes_at &&
+        navigate("/dashboard");
+    };
+
+    state && state.user && isPaused();
+  }, [state && state.user]);
+
+  const fetchPrices = async () => {
+    const { data } = await axios.get(
+      "https://pwo-mern-api.onrender.com/api/prices"
+    );
+    setPrices(data);
+    setIsFetching(false);
+  };
+
+ 
   return (
     <div className="w-full h-full flex flex-col justify-center overflow-hidden relative  pt-[6.2rem] pb-10 ">
       <SubscriptionPlan />
